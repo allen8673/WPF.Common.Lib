090016271f8fcb9a93ecddad4ad5ea8ff02a56db
WHQ\10704306 <Allen_CL_Lin@wistron.com>
2019-06-18 18:54:42 +0800

    Commit style

---
+++ WPF.Common.Styles/Converter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPF.Common.Styles
+{
+    public class ToolTipPlacementConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.FirstOrDefault(v => v == DependencyProperty.UnsetValue) != null)
+            {
+                return double.NaN;
+            }
+
+            double placementTargetHeight = (double)values[0];
+            double toolTipHeight = (double)values[1];
+            return Math.Abs((placementTargetHeight / 2.0) + (toolTipHeight / 2.0)) * -1;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
